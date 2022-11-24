195cf1f9fcc4129903368092f59601668325fb04
Ingvar Derešivski <inde999@gmail.com>
2022-11-24 11:30:14 +0200

    DbContextSpaceshipMigration

--- TARge21Shop/TARge21Shop/Program.cs
+++ TARge21Shop/TARge21Shop/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TARge21Shop.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TARge21ShopContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
