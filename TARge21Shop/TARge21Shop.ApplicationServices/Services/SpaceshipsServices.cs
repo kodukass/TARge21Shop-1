1089a9b7c7cdb1c814eaffb2a4229b5097fde830
Ingvar Derešivski <inde999@gmail.com>
2022-12-08 11:35:04 +0200

    SpaceshipUpdate

--- TARge21Shop/TARge21Shop.ApplicationServices/Services/SpaceshipsServices.cs
+++ TARge21Shop/TARge21Shop.ApplicationServices/Services/SpaceshipsServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -48,5 +49,43 @@ namespace TARge21Shop.ApplicationServices.Services
 
             return domain;
         }
+
+
+        public async Task<Spaceship> Update(SpaceshipDto dto)
+        {
+            var domain = new Spaceship()
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Type = dto.Type,
+                Crew = dto.Crew,
+                Passengers = dto.Passengers,
+                CargoWeight = dto.CargoWeight,
+                FullTripsCount = dto.FullTripsCount,
+                MaintenanceCount = dto.MaintenanceCount,
+                LastMaintenance = dto.LastMaintenance,
+                EnginePower = dto.EnginePower,
+                MaidenLaunch = dto.MaidenLaunch,
+                BuiltDate = dto.BuiltDate,
+                CreatedAt = dto.CreatedAt,
+                ModifiedAt = DateTime.Now,
+            };
+
+            _context.Spaceships.Update(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+        }
+
+        public async Task<Spaceship> GetUpdate(Guid id)
+        {
+            var result = await _context.Spaceships
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+
+
+        
     }
 }
--- TARge21Shop/TARge21Shop.Core/ServiceInterface/ISpaceshipsServices.cs
+++ TARge21Shop/TARge21Shop.Core/ServiceInterface/ISpaceshipsServices.cs
@@ -6,5 +6,7 @@ namespace TARge21Shop.Core.ServiceInterface
     public interface ISpaceshipsServices
     {
         Task<Spaceship> Add(SpaceshipDto dto);
+        Task<Spaceship> GetUpdate(Guid id);
+        Task<Spaceship> Update(SpaceshipDto dto);
     }
 }
