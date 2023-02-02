831b20b76e8081c2b3633351d656dd4af2323b03
Emma :) <99496596+kodukass@users.noreply.github.com>
2023-02-02 09:59:50 +0200

    create update

--- TARge21Shop/TARge21Shop/Controllers/RealEstatesController.cs
+++ TARge21Shop/TARge21Shop/Controllers/RealEstatesController.cs
@@ -1,25 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TARge21Shop.Core.Domain;
+using TARge21Shop.Core.Dto;
 using TARge21Shop.Core.ServiceInterface;
+using TARge21Shop.Data;
 using TARge21Shop.Models.RealEstate;
 
 namespace TARge21Shop.Controllers
 {
     public class RealEstatesController : Controller
     {
-        private readonly IRealEstatesServices _realEstates;
+        private readonly IRealEstatesServices _realEstatesServices;
+        private readonly TARge21ShopContext _context;
 
         public RealEstatesController
             (
-                IRealEstatesServices realEstates
+                IRealEstatesServices realEstatesServices,
+                TARge21ShopContext context
             )
         {
-            _realEstates = realEstates;
+            _realEstatesServices = realEstatesServices;
+            _context = context;
         }
 
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
+        //[HttpGet]
+        //public async Task<IActionResult> Index()
+        //{
             //var realEstate = await _realEstates.GetAsync();
 
             //if (realEstate == null)
@@ -27,7 +33,7 @@ namespace TARge21Shop.Controllers
             //    return NotFound();
             //}
 
-            var vm = new RealEstateIndexViewModel();
+            //var vm = new RealEstateIndexViewModel();
 
             //vm.Id = id;
             //vm.Address = realEstate.Address;
@@ -44,7 +50,61 @@ namespace TARge21Shop.Controllers
             //vm.ModifiedAt = realEstate.ModifiedAt;
             //vm.CreatedAt = realEstate.CreatedAt;
 
-            return View(vm);
+            //return View(vm);
+       // }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var result = _context.RealEstates
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new RealEstateIndexViewModel
+                {
+                    Id = x.Id,
+                    Address = x.Address,
+                    City = x.City,
+                    Country = x.Country,
+                    Size = x.Size,
+                    Price = x.Price,
+                });
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            RealEstateCreateUpdateViewModel vm = new RealEstateCreateUpdateViewModel();
+
+            return View("CreateUpdate", vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RealEstateCreateUpdateViewModel vm)
+        {
+            var dto = new RealEstateDto()
+            {
+                Id = vm.Id,
+                Address = vm.Address,
+                City = vm.City,
+                Country = vm.Country,
+                Size=vm.Size,
+                Price=vm.Price,
+                Floor = vm.Floor,
+                Region = vm.Region,
+                Phone = vm.Phone,
+                Fax = vm.Fax,
+                PostalCode = vm.PostalCode,
+                RoomCount = vm.RoomCount,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt
+            };
+            var result = await _realEstatesServices.Create(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction("Index", vm);
         }
     }
 }
+            
